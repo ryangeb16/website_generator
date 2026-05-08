a0749c653a4b0810c463e7aa996cc1fe0e5bf7c5
github_ryangeb16 <ryangeb16@gmail.com>
2026-05-07 19:47:54 -0600

    html nodes almost done.

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -34,3 +34,21 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError
+        if self.children is None:
+            raise ValueError("Children cannot be 'None' for ParentNode")
+        inner_html = ""
+        for child in self.children:
+            inner_html += child.to_html()
+        html_string = f'<{self.tag}{self.props_to_html()}>{inner_html}</{self.tag}>'
+        return html_string
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode,ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -35,4 +35,19 @@ class TestHTMLLeaf(unittest.TestCase):
     def test_leaf_to_html_props(self):
         node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
         self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+class TestHTMLParent(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
 
